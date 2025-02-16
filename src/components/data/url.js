4fd80fc0f4ae40643e193ab3f32a7eb3cbafc1a7
Abhay Singh <singhabhay3145@gmail.com>
2025-02-16 23:43:13 +0530

    Update url.js

--- src/components/data/url.js
+++ src/components/data/url.js
@@ -1,4 +1,4 @@
-export const url="https://club-notification-backend-1.onrender.com"
+export const url="https://club-notification-backend-1.onrender.com" // Your backend domain
 export const url1="http://127.0.0.1:5001"
 export const url2="http://127.0.0.2:5002"
-export const websocket="https://club-notification-backend-1.onrender.com"
+export const websocket="https://club-notification-backend-1.onrender.com" // Your backend domain
